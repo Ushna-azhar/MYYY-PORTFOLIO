a089157945e15de1872a5330a20fdc48ee466d60
Ushna-azhar <773.ushnaazhar@gmail.com>
2024-11-17 21:24:30 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,17 @@
 import HeroSection from "../components/HeroSection";
 import Navbar from '../components/Navbar';
-
 import Footer from '../components/Footer';  // Import Footer component
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black text-white">
       
-      <section className="px-8 py-4 mx-auto">
+      {/* HeroSection Wrapper */}
+      <section className="px-8 py-4 mx-auto flex-grow">
         <HeroSection />
       </section>
 
-     
-
+      {/* Footer */}
       <Footer />
     </main>
   );
